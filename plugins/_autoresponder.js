46a9c4cb9a0ba264b7a77406fe638ff705054960
عبدالكريم معاذ محمد علي البكاري <202470349@su.edu.ye>
2025-08-20 21:51:29 +0300

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -17,7 +17,7 @@ return true
 }
   
 //if (m.mentionedJid.includes(this.user.jid) || (m.quoted && m.quoted.sender === this.user.jid) && !chat.isBanned) {
-if (m.mentionedJid.includes(this.user.jid)) {
+if (await m.mentionedJid.includes(this.user.jid)) {
 if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||  m.text.includes('menu') ||  m.text.includes('estado') || m.text.includes('bots') ||  m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('Video') || m.text.includes('Audio') || m.text.includes('audio')) return !0
 
 async function luminsesi(q, username, logic) {
