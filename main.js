d676b968e92e456dad9bb6bdaa2975a439006bcf
عبدالكريم معاذ محمد علي البكاري <202470349@su.edu.ye>
2025-08-21 22:07:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ global.__filename = function filename(pathURL = import.meta.url, rmPrefix = plat
 global.timestamp = { start: new Date }
 const __dirname = global.__dirname(import.meta.url);
 global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
-global.prefix = new RegExp('^[' + (opts['prefix'] || '*/i!#$%+£¢€¥^°=¶∆×÷π√✓©®&.\\-.@').replace(/[|\\{}()[\]^$+*.\-\^]/g, '\\$&') + ']')
+global.prefix = new RegExp('^[#!/.]');
 
 global.db = new Low(/https?:\/\//.test(opts['db'] || '') ? new cloudDBAdapter(opts['db']) : new JSONFile('database.json'))
 global.DATABASE = global.db; 
